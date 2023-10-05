25b60aa05586966ca4a8465abcadfbcd803ac2cd
Brian Cox <brianthony.cox88.com>
2023-10-04 22:28:08 -0500

    changed break to comment

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 # my_input = input('what is your input ')
 # print(my_input)
 
-x = 0
+# x = 0
 
-while x <= 100:
-    print(x)
-    x += 1
+# while x <= 100:
+#     print(x)
+#     x += 1
+
+while True:
+    my_name = input('What is your name?\n')
+    if my_name.lower()[0] in 'abcdefg':
+        print('Please procede to table One')
+    elif my_name.lower()[0] in 'hijklmnop':
+        print("Please procede to table Two")
+    elif my_name.lower()[0] in 'qrstuvwxyz':
+        print('Please proceed to table Three')
+    elif my_name.lower() == 'quit':
+        # break
+        
